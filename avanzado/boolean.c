c1cd28c18ecf119e94967b753c6673b2aa82b50c
VERONA\verona <lau.marzet@gmail.com>
2018-02-25 02:55:08 -0600

    nueva estructura

--- avanzado/boolean.c
+++ avanzado/boolean.c
@@ -2,27 +2,67 @@
 #include<stdlib.h>
 #include<stdbool.h>
 
-int main(){
+int main() {
 
-char respuesta;
+    char expresion[100] = "(V|V)&F&(F|V)";
+    int i, j,k;
+    int contador;
+    int resultado[100];
+    int parentesis[100];
+    int fueraparen[100];
 
-char cadena[5];
-cadena[0] = 'V';
-cadena[1] = 'V';
-cadena[2] = 'F';
-cadena[3] = 'F';
-cadena[4] = 'V';
-int i;
-
-for(i=0;i<5;i++){
-    if(cadena[i] == 'V'){
-        respuesta = true;
-        printf("True");
-    }else{
-        respuesta = false;
-        printf("False");
+    for (i = 0; i < 100; i++) {
+        switch (expresion[i]) {
+            case'F':
+                resultado[i] = 0;
+                break;
+            case'V':
+                resultado[i] = 1;
+                break;
+            case'|':
+                resultado[i] = 2;
+                break;
+            case'&':
+                resultado[i] = 3;
+                break;
+            case'!':
+                resultado[i] = 4;
+                break;
+            case'(':
+                resultado[i] = 5;
+                break;
+            case')':
+                resultado[i] = 6;
+                break;
+            case' ':
+                resultado[i] = 7;
+                break;
+        }
     }
-}
+
+    for (j = 0; j < 100; j++) {
+        contador = j;
+        if (resultado[j] == 5) {
+            do {
+                parentesis[contador] = resultado[contador];
+                //printf("%s",&expresion[contador]);
+                contador++;
+            } while (resultado[contador - 1] != 6);
+        } else {
+            fueraparen[j] = resultado[j];
+            
+        }
+    }
+    
+    for(k=0;k<100;k++){
+/*
+        if(resultado[k] > 7 || resultado[k] < 0){
+            break;
+        }
+*/
+        printf("%d",parentesis[k]);
+    }
 
     return 0;
 }
+
